9d5e157cb83df1df6a3732a1e90913c5f3b5cc6a
Sachin Neravath <sachi77n@gmail.com>
2020-05-03 17:54:36 +0530

    Version 1.2.0 release

--- dist/lg-fullscreen.js
+++ dist/lg-fullscreen.js
@@ -1,6 +1,6 @@
-/*! lg-fullscreen - v1.1.0 - 2019-02-19
+/*! lg-fullscreen - v1.2.0 - 2020-05-03
 * http://sachinchoolur.github.io/lightGallery
-* Copyright (c) 2019 Sachin N; Licensed GPLv3 */
+* Copyright (c) 2020 Sachin N; Licensed GPLv3 */
 
 (function (root, factory) {
   if (typeof define === 'function' && define.amd) {
@@ -59,7 +59,7 @@
                 !document.mozFullScreenEnabled && !document.msFullscreenEnabled) {
                 return;
             } else {
-                fullScreen = '<span class="lg-fullscreen lg-icon"></span>';
+                fullScreen = '<button aria-label="Toggle fullscreen" class="lg-fullscreen lg-icon"></button>';
                 this.core.$outer.find('.lg-toolbar').append(fullScreen);
                 this.fullScreen();
             }
