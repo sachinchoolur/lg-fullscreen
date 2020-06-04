670435fb1a809a2d31de85d1e1811e5ef98d9a85
Sachin Neravath <sachi77n@gmail.com>
2020-06-04 22:28:03 +0530

    accessibility - add button type

--- src/lg-fullscreen.js
+++ src/lg-fullscreen.js
@@ -39,7 +39,7 @@
                 !document.mozFullScreenEnabled && !document.msFullscreenEnabled) {
                 return;
             } else {
-                fullScreen = '<button aria-label="Toggle fullscreen" class="lg-fullscreen lg-icon"></button>';
+                fullScreen = '<button type="button" aria-label="Toggle fullscreen" class="lg-fullscreen lg-icon"></button>';
                 this.core.$outer.find('.lg-toolbar').append(fullScreen);
                 this.fullScreen();
             }
